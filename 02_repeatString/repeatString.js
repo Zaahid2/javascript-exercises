7a9d98920839ff5402d0b72edb2030e2665d0000
Zaahid Cader <136280987+Zaahid2@users.noreply.github.com>
2023-12-05 23:54:16 +0000

    Completed ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if(times<0) return "Error";
+    sentence = "";
+    for(count = 0; count < times; count++) {
+        sentence += word;
+    }
+    return sentence;
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
