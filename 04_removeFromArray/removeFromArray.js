c24569dccc5de8046cc517c6fb2134cc0087d613
Zaahid Cader <136280987+Zaahid2@users.noreply.github.com>
2023-12-09 01:28:51 +0000

    Completed ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, index) {
+    array.splice(index-1,1);
+    return array;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
